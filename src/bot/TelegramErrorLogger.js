64e6650b0c85be64890d09c05bf886792369d3bc
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-11-19 15:00:42 +0200

    Log errors to debug chat

---
+++ src/bot/TelegramErrorLogger.js
@@ -0,0 +1,24 @@
+export class TelegramErrorLogger {
+  /**
+   * @param {{
+   *   telegram: import('telegraf').Telegram,
+   *   debugChatId: string,
+   * }} param0 
+   */
+  constructor({ telegram, debugChatId }) {
+    this._debugChatId = debugChatId
+    this._telegram = telegram
+  }
+
+  log(error, message = 'Unexpected error', context = {}) {
+    this._telegram.sendMessage(
+      this._debugChatId,
+      [
+        `❗️ ${new Date().toISOString().replace('T', ' ').replace('Z', '')} ${message}:`,
+        String(error.stack) || `${error.name}: ${error.message}`,
+        `Context:`,
+        `${JSON.stringify(context)}`
+      ].join('\n')
+    ).catch(error => console.error({ error }, 'Could not log to the debug chat'))
+  }
+}
