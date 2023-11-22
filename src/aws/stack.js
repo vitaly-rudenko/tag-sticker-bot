1b4391f396afed117da7d1606ac4193538689bcd
Vitaly Rudenko <maurice.dederick@gmail.com>
2023-11-22 13:39:23 +0200

    Revert kebab-case

--- src/aws/stack.js
+++ src/aws/stack.js
@@ -27,7 +27,7 @@ export class TagStickerBotStack extends cdk.Stack {
     const userSessionsTable = this.createUserSessionsTable()
     const tagsTable = this.createTagsTable()
 
-    const restApiLambda = new cdk.aws_lambda.Function(this, 'rest-api-lambda', {
+    const restApiLambda = new cdk.aws_lambda.Function(this, 'restApiLambda', {
       code: cdk.aws_lambda.Code.fromAsset(path.join(root, 'dist', 'rest-api')),
       handler: 'index.handler',
       runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
@@ -51,7 +51,7 @@ export class TagStickerBotStack extends cdk.Stack {
 
     const lambdaIntegration = new cdk.aws_apigateway.LambdaIntegration(restApiLambda)
 
-    const restApi = new cdk.aws_apigateway.RestApi(this, 'rest-api', {
+    const restApi = new cdk.aws_apigateway.RestApi(this, 'restApi', {
       deployOptions: {
         stageName: environment,
       }
@@ -65,7 +65,7 @@ export class TagStickerBotStack extends cdk.Stack {
     const webhookUrl = `${restApi.url}webhook`
     const debugUrl = `${restApi.url}debug`
 
-    const initLambda = new cdk.aws_lambda.Function(this, 'init-lambda', {
+    const initLambda = new cdk.aws_lambda.Function(this, 'initLambda', {
       code: cdk.aws_lambda.Code.fromAsset(path.join(root, 'dist', 'init')),
       handler: 'index.handler',
       runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
@@ -80,19 +80,19 @@ export class TagStickerBotStack extends cdk.Stack {
       },
     })
 
-    new cdk.triggers.Trigger(this, 'init-trigger', {
+    new cdk.triggers.Trigger(this, 'initTrigger', {
       handler: initLambda,
       invocationType: cdk.triggers.InvocationType.REQUEST_RESPONSE,
       timeout: cdk.Duration.minutes(1),
     })
 
-    new cdk.CfnOutput(this, 'health-url', { value: healthUrl })
-    new cdk.CfnOutput(this, 'webhook-url', { value: webhookUrl })
-    if (!isProduction) new cdk.CfnOutput(this, 'debug-url', { value: debugUrl })
+    new cdk.CfnOutput(this, 'healthUrl', { value: healthUrl })
+    new cdk.CfnOutput(this, 'webhookUrl', { value: webhookUrl })
+    if (!isProduction) new cdk.CfnOutput(this, 'debugUrl', { value: debugUrl })
   }
 
   createTagsTable() {
-    const table = new cdk.aws_dynamodb.Table(this, 'tags-table', {
+    const table = new cdk.aws_dynamodb.Table(this, 'tagsTable', {
       partitionKey: {
         name: tagAttributes.tagId,
         type: cdk.aws_dynamodb.AttributeType.STRING
@@ -144,7 +144,7 @@ export class TagStickerBotStack extends cdk.Stack {
   }
 
   createUserSessionsTable() {
-    return new cdk.aws_dynamodb.Table(this, 'user-sessions-table', {
+    return new cdk.aws_dynamodb.Table(this, 'userSessionsTable', {
       partitionKey: {
         name: userSessionAttributes.userId,
         type: cdk.aws_dynamodb.AttributeType.STRING
