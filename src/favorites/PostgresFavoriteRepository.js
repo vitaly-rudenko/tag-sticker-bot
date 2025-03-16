f0631c3ead736a1422b4fac3964b8e1930bd767c
Vitaly Rudenko <maurice.dederick@gmail.com>
2025-03-16 14:30:15 +0200

    Use generateQuery() in Favorites repo

--- src/favorites/PostgresFavoriteRepository.js
+++ src/favorites/PostgresFavoriteRepository.js
@@ -1,10 +1,12 @@
+import { generateQuery } from '../utils/generate-query.js'
+
 export class PostgresFavoriteRepository {
   /**
    * @param {{
    *   postgresClient: import('pg').Client
    * }} options
    */
-  constructor({ postgresClient })  {
+  constructor({ postgresClient }) {
     this._postgresClient = postgresClient
   }
 
@@ -16,11 +18,19 @@ export class PostgresFavoriteRepository {
    */
   async mark({ userId, file }) {
     await this._postgresClient.query(
-      `INSERT INTO favorites (user_id, file_id, file_unique_id, set_name, mime_type)
-       VALUES ($1, $2, $3, $4, $5)
-       ON CONFLICT DO NOTHING;`,
-      [userId, file.file_id, file.file_unique_id, file.set_name, file.mime_type]
-    )
+      ...generateQuery(
+        `INSERT INTO favorites (user_id, file_id, file_unique_id, set_name, mime_type)
+         VALUES (:userId, :fileId, :fileUniqueId, :setName, :mimeType)
+         ON CONFLICT DO NOTHING;`,
+        {
+          userId,
+          fileId: file.file_id,
+          fileUniqueId: file.file_unique_id,
+          setName: file.set_name,
+          mimeType: file.mime_type,
+        }
+      )
+    );
   }
 
   /**
@@ -31,9 +41,11 @@ export class PostgresFavoriteRepository {
    */
   async unmark({ userId, fileUniqueId }) {
     await this._postgresClient.query(
-      `DELETE FROM favorites
-       WHERE user_id = $1 AND file_unique_id = $2;`,
-      [userId, fileUniqueId]
+      ...generateQuery(
+        `DELETE FROM favorites
+         WHERE user_id = :userId AND file_unique_id = :fileUniqueId;`,
+        { userId, fileUniqueId }
+      )
     )
   }
 
@@ -47,16 +59,18 @@ export class PostgresFavoriteRepository {
    */
   async query({ userId, limit, fromFileUniqueId }) {
     const { rows } = await this._postgresClient.query(
-      `SELECT file_id, file_unique_id, set_name, mime_type
-       FROM favorites
-       WHERE user_id = $1
-       ${fromFileUniqueId ? 'AND file_unique_id > $3' : ''}
-       ORDER BY id DESC
-       LIMIT $2;`,
-      [userId, limit, fromFileUniqueId]
-    )
+      ...generateQuery(
+        `SELECT file_id, file_unique_id, set_name, mime_type
+         FROM favorites
+         WHERE user_id = :userId
+         ${fromFileUniqueId ? 'AND file_unique_id > :fromFileUniqueId' : ''}
+         ORDER BY id DESC
+         LIMIT :limit;`,
+        { userId, limit, fromFileUniqueId }
+      )
+    );
 
-    return rows.map(row => ({
+    return rows.map((row) => ({
       file_id: row.file_id,
       file_unique_id: row.file_unique_id,
       set_name: row.set_name,
@@ -72,11 +86,13 @@ export class PostgresFavoriteRepository {
    */
   async isMarked({ userId, fileUniqueId }) {
     const { rows } = await this._postgresClient.query(
-      `SELECT 1
-       FROM favorites
-       WHERE user_id = $1 AND file_unique_id = $2;`,
-      [userId, fileUniqueId]
-    )
+      ...generateQuery(
+        `SELECT 1
+         FROM favorites
+         WHERE user_id = :userId AND file_unique_id = :fileUniqueId;`,
+        { userId, fileUniqueId }
+      )
+    );
 
     return rows.length > 0
   }
