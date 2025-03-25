5fda8f2c1e0d3c1c9e2ba9a5cca8328175af6cc2
Vitaly Rudenko <maurice.dederick@gmail.com>
2025-03-25 03:06:48 +0200

    Remove commands

--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,7 @@ const filesRepository = new FilesRepository({ client: postgresClient })
 const stickerSetsRepository = new StickerSetsRepository({ client: postgresClient })
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!)
+await bot.telegram.setMyCommands([])
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
